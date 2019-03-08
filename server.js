0a61a222badcd00f7810de5e14c0327933c21c3b
tomomoto <venom369@yandex.ru>
2019-03-08 21:03:47 +0300

    Just refactoring.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-var orm = require('orm');
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io').listen(server);
+let orm = require('orm');
 
 users = [];
 connections = [];
@@ -13,12 +13,12 @@ connections = [];
 //178.252.127.246
 //server.listen(process.env.PORT || 3000, 'localhost');
 //server.listen(process.env.PORT || 3000, '178.252.118.52');
-var port = process.env.PORT;
+let port = process.env.PORT;
 if (process.env.PORT == null)
     port = 3000;
 //var host = '0.0.0.0';
 //var host = '178.252.118.52';
-var host = '10.137.57.156';
+const host = '10.137.57.156';
 server.listen(port, host);
 //server.listen(process.env.PORT || 3000, '10.137.57.156');
 
@@ -43,8 +43,8 @@ io.use(orm.express("mysql://root:welldone@127.0.0.1/sppr", {
 }));
 
 function defineEvents(models, db) {
-    models.Events = db.define("events", {
-        event_id: {type: 'serial', key: true},
+    let eventsModel = {
+        id: {type: 'serial', key: true},
         name: String,
         description: String,
         creator_id: {type: "integer"},
@@ -54,21 +54,22 @@ function defineEvents(models, db) {
         city: String,
         latitude: {type: "number"},
         longitude: {type: "number"}
-    });
+    };
+    models.Events = db.define("events", eventsModel);
 }
 
 function defineUsers(models, db) {
-    models.Users = db.define("users", {
-        user_id: {type: 'serial', key: true},
-        login : String,
-        //login : String,
+    let userModel = {
+        id: {type: 'serial', key: true},
+        login: String,
         name: String,
         surname: String,
-        sex: ["male", "female"],
+        gender: ["male", "female"],
         info: String,
         photo: Buffer,
         birthday: {type: "date", time: false}
-    });
+    };
+    models.Users = db.define("users", userModel);
 }
 
 io.use(function (socket, next) {
@@ -84,7 +85,7 @@ io.use(function (socket, next) {
 let i = 1;
 
 app.get('/hello', function (req, res) {
-    console.log("sow " +i);
+    console.log("sow " + i);
     i++;
     //res.send(__dirname);
     res.send("Hello");
@@ -95,10 +96,38 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/users/{id}', function (req, res) {
-    //res.send(__dirname);
-    res.sendFile(__dirname + '/index.html');
+app.get('/user/:userId', function (req, res) {
+    req.models.Users.one({id: req.params.userId}, function (err, user) {
+        if (err) {
+            res.writeHead(404, {"Content-Type": "application/json"});
+            throw err;
+        }
+        else {
+            if (user == null) {
+                //res.writeHead(404, {"Content-Type": "application/json"});
+                res.send("");
+            }
+            else {
+                res.send(user);
+            }
+        }
+    });
 });
+
+let rad = function (x) {
+    return x * Math.PI / 180;
+};
+
+let getDistance = function (p1, p2) {
+    const R = 6378137; // Earth's mean radius in meter
+    const dLat = rad(p2.latitude - p1.latitude);
+    const dLong = rad(p2.longitude - p1.longitude);
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(rad(p1.latitude)) * Math.cos(rad(p2.latitude)) *
+        Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c; // returns the distance in meter
+};
 
 app.get('/events', function (req, res) {
     req.models.Events.find({}, function (err, events) {
@@ -107,12 +136,12 @@ app.get('/events', function (req, res) {
             throw err;
         }
         else {
-            if (events.length == 0) {
+            if (events.length === 0) {
                 socket.emit("FoundNullEvents", events.length);
             }
             else {
                 let eventList = [];
-                for (var i = 0; i < events.length; i++) {
+                for (let i = 0; i < events.length; i++) {
                     let dist = getDistance(new LatLng(req.query.latitude, req.query.longitude), events[i]);
                     let parsed = parseInt(req.query.area);
                     if (dist < req.query.area)
@@ -132,42 +161,28 @@ let LatLng = class {
     }
 };
 
-var rad = function (x) {
-    return x * Math.PI / 180;
-};
-
-var getDistance = function (p1, p2) {
-    var R = 6378137; // Earth�s mean radius in meter
-    var dLat = rad(p2.latitude - p1.latitude);
-    var dLong = rad(p2.longitude - p1.longitude);
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(rad(p1.latitude)) * Math.cos(rad(p2.latitude)) *
-        Math.sin(dLong / 2) * Math.sin(dLong / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return R * c; // returns the distance in meter
-};
 
 io.sockets.on('connection', function (socket) {
-    var onUserLogin_Cb = function (data) {
+    let onUserLoginCallback = function (data) {
         console.log("Login event: " + data.login + ' ' + data.password);
-        if (check_incoming_data(data)) {
-            socket.models.Users.find({login: data.login, password: data.password}, function (err, users) {
+        if (checkIncomingData(data)) {
+            socket.models.Users.one({login: data.login, password: data.password}, function (err, user) {
                 if (err) {
                     throw err;
                 }
                 else {
-                    if (users.length == 0)
+                    if (user == null)
                         socket.emit("WrongLoginEvent", data);
                     else {
-                        console.log("User logged in: %s %s", users[0].name, users[0].surname);
+                        console.log("User logged in: %s %s", user.name, user.surname);
                         //var birth = new Date(users[0].birthday).format("yyyy-mm-dd");
                         //var birth_month = birth.getMonth() + 1;
                         //var formattedBirthDay = birth.getDate()+'.'+ birth_month+'.'+birth.getFullYear();
-                       // var formated_date = now.format("yyyy-mm-dd");
+                        // var formated_date = now.format("yyyy-mm-dd");
                         //users[0].birthday = formattedBirthDay;
 
-                        socket.usr = users[0].user_id;
-                        socket.emit("RightLoginEvent", users[0]);
+                        socket.usr = user.id;
+                        socket.emit("RightLoginEvent", user);
                     }
                 }
             });
@@ -177,14 +192,14 @@ io.sockets.on('connection', function (socket) {
         }
     };
 
-    var onSocketDisconnection_Cb = function () {
+    let onSocketDisconnectedCallback = function () {
         users.splice(users.indexOf(socket.username), 1);
-        updateUsernames();
+        updateUserNames();
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected: %s sockets connected', connections.length);
     };
 
-    var onFindEvents_Cb = function (userProps) {
+    let onFindEventsCallback = function (userProps) {
         console.log(userProps);
         socket.models.Events.find({
             //latitude: orm.between(userProps.latitude - userProps.area, userProps.latitude + userProps.area),
@@ -195,12 +210,12 @@ io.sockets.on('connection', function (socket) {
                 throw err;
             }
             else {
-                if (events.length == 0) {
+                if (events.length === 0) {
                     socket.emit("FoundNullEvents", events.length);
                 }
                 else {
                     let eventList = [];
-                    for (var i = 0; i < events.length; i++) {
+                    for (let i = 0; i < events.length; i++) {
                         let dist = getDistance(new LatLng(userProps.latitude, userProps.longitude), events[i]);
                         if (dist < userProps.area)
                             eventList.push(events[i]);
@@ -214,33 +229,31 @@ io.sockets.on('connection', function (socket) {
     };
 
     //TODO Event creation
-    var onEventCreation_Cb = function (newEventData) {
+    let onEventCreationCallback = function (newEventData) {
 
     };
 
     //TODO USERREG
-    var onUserRegistration_Cb = function (newUserData) {
+    let onUserRegistrationCallback = function (newUserData) {
 
     };
 
-    var initialize = function (socket) {
-        socket.on('disconnect', onSocketDisconnection_Cb);
-        socket.on('login', onUserLogin_Cb);
-        socket.on('EventCreation', onEventCreation_Cb);
-        socket.on('FindEvents', onFindEvents_Cb);
-        socket.on('UserRegistration', onUserRegistration_Cb);
+    let initialize = function (socket) {
+        socket.on('disconnect', onSocketDisconnectedCallback);
+        socket.on('login', onUserLoginCallback);
+        socket.on('EventCreation', onEventCreationCallback);
+        socket.on('FindEvents', onFindEventsCallback);
+        socket.on('UserRegistration', onUserRegistrationCallback);
         connections.push(socket);
         console.log('Connected: %s sockets connected', connections.length);
     };
     initialize(socket);
 });
 
-function updateUsernames() {
+function updateUserNames() {
     io.sockets.emit('get users', users);
 }
-function check_incoming_data(data) {
-    if (data.login && data.password)
-        return true;
-    else
-        return false;
+
+function checkIncomingData(data) {
+    return !!(data.login && data.password);
 }
